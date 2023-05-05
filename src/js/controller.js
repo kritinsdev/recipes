b72e6f90b910d2e9bfa0c0fcc8824a66d7859033
Kristaps Ritins <kritinsdev@gmail.com>
2023-05-06 01:07:04 +0300

    helpers and config

---
+++ src/js/controller.js
@@ -0,0 +1,24 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    await model.loadRecipe(id);
+
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    alert(error)
+  }
+}
+
+const events = ['load', 'hashchange'];
+events.forEach(event => window.addEventListener(event, controlRecipes));
+
